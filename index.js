64c05d0e4fb116d32de1d2d81dafb83293d140f4
Abu Sayed <rafsansayed132@gmail.com>
2022-11-09 19:36:02 +0600

    added delete review

--- index.js
+++ index.js
@@ -86,12 +86,12 @@ async function run(){
             res.send(reviews)
         });
 
-        // app.delete('/reviews/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await reviewsCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // app.get('/reviews', verifyJWT, async(req, res)=>{
         //     const decoded = req.decoded;
